f6f7f7178c9b507384c1fcdcd77290b5e52fac63
berkbilir <berkbilir@sabanciuniv.edu>
2019-12-10 17:02:11 +0300

    removed unnecessary merkle tree implementation

--- PoW.py
+++ PoW.py
@@ -1,52 +1,5 @@
 from Crypto.Hash import SHA3_256
 
-def merkleRoot(storeHash):
-
-	result = []
-
-	temp = []
-
-	n = 2
-	storeHash = [storeHash[k:k+n] for k in range(0, len(storeHash), n)]
-
-	j = 0
-
-	for i in storeHash:
-
-		temp = storeHash[i]
-
-		storeHash[i] = temp[j] + temp[j+1]
-
-		h2 = SHA3_256.new()
-		h2.update(storeHash[i].encode("UTF-8"))
-
-		result.append(h2)
-
-		temp = []
-
-		
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 def CheckPow(p, q, g, nonce_lenght, TxCnt, file):
 
@@ -68,7 +21,6 @@ def CheckPow(p, q, g, nonce_lenght, TxCnt, file):
 	
 		storeHash.append(h.hexdigest())
 
-	merkleRoot(storeHash)
 
 	#Generate Root Hash
 	while len(storeHash) > 1:
@@ -93,13 +45,18 @@ def CheckPow(p, q, g, nonce_lenght, TxCnt, file):
 
 		del storeHash[-lastDelete:];
 
-	h3 = SHA3_256.new()
-
-	h3.update((storeHash[0] + "277157211575692414559330522236021589363").encode("UTF-8"))
-
 	
 
+	h3 = SHA3_256.new()
+
+	nonce = 277157211575692414559330522236021589363
+
+	digest = (storeHash[0] + (str(nonce)+'\n')).encode("UTF-8")
+
+	h3.update(digest)
+
 	return h3.hexdigest()
+
 
 
 
