c2ebb60e9227b6cd17e31df3b3d80716b830ec82
berkbilir <berkbilir@sabanciuniv.edu>
2019-12-11 00:31:28 +0300

    removed newline from tx

--- PoW.py
+++ PoW.py
@@ -13,19 +13,22 @@ def CheckPow(p, q, g, nonce_lenght, TxCnt, file):
 
 	for i in range(0, TxCnt):
 		
-		tx = "".join(block[i*7: i*7+5])
+		tx = "".join(block[i*7: i*7+5]).rstrip()
+
+		print(tx)
 		
 		h = SHA3_256.new()
 		
 		h.update(tx.encode("UTF-8"))
 	
-		storeHash.append(h.hexdigest())
+		storeHash.append(h.digest())
+
 
 
 	#Generate Root Hash
 	while len(storeHash) > 1:
 
-
+		
 		j = 0
 
 		for i in range(0, len(storeHash) - 1): 
@@ -33,9 +36,9 @@ def CheckPow(p, q, g, nonce_lenght, TxCnt, file):
 			concat = storeHash[i] + storeHash[i+1]
 
 			h2 = SHA3_256.new()
-			h2.update(concat.encode("UTF-8"))
+			h2.update(concat)
 
-			storeHash[j] = h2.hexdigest()
+			storeHash[j] = h2.digest()
 			
 
 			i += 2
@@ -45,13 +48,12 @@ def CheckPow(p, q, g, nonce_lenght, TxCnt, file):
 
 		del storeHash[-lastDelete:];
 
-	
 
 	h3 = SHA3_256.new()
 
 	nonce = 277157211575692414559330522236021589363
 
-	digest = (storeHash[0] + (str(nonce)+'\n')).encode("UTF-8")
+	digest = storeHash[0] + ((str(nonce)+'\n')).encode("UTF-8")
 
 	h3.update(digest)
 
